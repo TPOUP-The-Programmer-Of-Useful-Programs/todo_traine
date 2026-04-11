530ab54e8470ba996b5aab98a81cd02d44fc74e9
Lucas <Lucas>
2026-04-11 16:54:42 -0300

    front end e api

--- api/server.js
+++ api/server.js
@@ -12,7 +12,23 @@ const pool = new pg.Pool({
     database: 'todo_app',
     user: 'postgres',
     password: 'root',
-})
+});
+    
+const initDb = async() => {
+    const queryText = `
+      CREATE TABLE IF NOT EXISTS tasks (
+        id SERIAL PRIMARY KEY,
+        title VARCHAR(255) NOT NULL,
+        description TEXT,
+        completed BOOLEAN DEFAULT false,
+        Created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      );
+    `;
+    await pool.query(queryText);
+    console.log("Tabela 'tasks' verificada/criada.");
+};
+
+initDb();
 
 pool.connect().then(() => {
     console.log('Conectado ao banco de dados PostgresSQL');
@@ -28,9 +44,15 @@ app.get('/tarefas', async function (req, res){
 })
 
 /* POST - CRIAR TAREFA */
-app.post('/tarefas', function (req, res){
-    res.json([]);
-})
+app.post('/tarefas', async function (req, res) {
+    const { title, description } = req.body;
+    const { rows: data } = await pool.query("INSERT INTO tasks (title, description) VALUES ($1, $2)", [
+        title,
+        description || null,
+    ]);
+
+    res.status(201).json({ data});
+});
 
 app.put('/tarefas', function (req, res){
     res.json([]);
---
+++ web/src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+const tarefas = [];
+
+function App() {
+  const [tarefas, setTarefas] = useState([]);
+
+  const adicionarItem = function (title, description) {
+    setTarefas((trf) => {
+      return [ {title, description }, ...trf];
+    })
+  };
+
+  const handleSubmit = function (e) {
+    e.preventDefault();
+
+    const title = e.target.title.value;
+    const description = e.target.description.value;
+
+    adicionarItem(title, description);
+    e.target.title.value = '';
+    e.target.description.value = '';
+  }
+
+
+  return (
+    <div>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlfor="title">Titulo:</label>
+            <input type="text" name="title" />
+          </div>
+          <div>
+            <label htmlfor="title">Descrição:</label>
+            <input type="text" name="description" />
+          </div>
+          <button type="submit">Salvar</button>
+        </form>
+      </div>
+      <div>
+        <ul>
+          {tarefas.map((item) => <li key={item.title}>{item.title}</li> )}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
